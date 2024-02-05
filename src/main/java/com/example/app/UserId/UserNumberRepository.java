9e59887d0a650c91c18f5797e1611a820d5f8305
dexy <v.cornici@gmail.com>
2024-02-05 18:33:02 +0200

    renaming User id to user number

---
+++ src/main/java/com/example/app/UserId/UserNumberRepository.java
@@ -0,0 +1,7 @@
+package com.example.app.UserId;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserNumberRepository extends CrudRepository<UserNumber, Long> {
+
+}
