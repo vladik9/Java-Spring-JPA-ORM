9624524227f1dbb7eea94bd787e5d8caae1f0e2f
Vlad <v.cornici@gmail.com>
2024-03-25 18:47:26 +0200

    adding empty project

--- src/main/java/com/example/app/AppApplication.java
+++ src/main/java/com/example/app/AppApplication.java
@@ -1,21 +1,14 @@
 package com.example.app;
-
-import com.example.app.Car.Car;
-import com.example.app.Car.CarRepository;
-import com.example.app.CarVIN.CarVIN;
-import com.example.app.CarVIN.CarVINRepository;
-import com.github.javafaker.Faker;
+ 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.dao.DataAccessException;
 
 @SpringBootApplication
 public class AppApplication {
-	
 
-	public static void main (String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
 
@@ -23,75 +16,9 @@ public class AppApplication {
 	// create a user and add it in DB, need to mark as bea as this will be used by
 	// Hibernate to identify it
 	@Bean
-	CommandLineRunner commandLineRunner(CarRepository carRepository, InputReader inputInputReader , CarVINRepository carVINRepository) {
+	CommandLineRunner commandLineRunner() {
 		return args -> {
-			int option=0;
-			do {
-				System.out.println("\nChoose a option your option:");
-				System.out.println("From Valid options:");
-				System.out.println("Option 1: Create and save a new car");
-				System.out.println("Option 2: Get a car by Id");
-				System.out.println("Option 0: Exit");
-				option = Integer.parseInt(inputInputReader.readInput());
-				System.out.println("You choose: " + option);
-				
-			switch (option) {
-					case 0:
-						System.exit(0);
-					break;
-					case 1: {
-						try {
-							carRepository.save(readInputCar(inputInputReader));
-						} catch (DataAccessException ex) {
-							System.out.println(ex.getCause().getMessage());
-						}
-						break;
-					}
-					case 2: {
-						try {
-							carRepository.findById(1L)
-									.
-							ifPresentOrElse((c -> System.out.println(c)),
-							()->{System.out.println("Not found");});
-						} catch (DataAccessException ex) {
-							System.out.println(ex.getCause().getMessage());
-						}
-						break;
-					}
-					default:
-					{
-						System.out.println(option+ " is not a valid option");
-					}
-					break;
-	}
-			} while (option != 0);
 		};
-
 	}
-	   // Assuming InputReader is in the same package or is component scanned
-		 @Bean
-			InputReader inputReader() {
-				return new InputReader();
-			}
-
-			private static Car readInputCar(InputReader inputInputReader) {
-				String name, color;
-				Integer year, number_of_seats;
-				name = inputInputReader.readInput("Enter car name: ");
-				color = inputInputReader.readInput("Enter car color:");
-				year = Integer.parseInt(inputInputReader.readInput("Enter car year: "));
-				number_of_seats = Integer.parseInt(inputInputReader.readInput("Enter car number of seats: "));
-				Faker faker = new Faker();
-				String VIN = faker.bothify("?????################").toUpperCase();
-				CarVIN carVIN = new CarVIN(VIN);
-				return new Car(name, color, year, number_of_seats, carVIN );
-			}
-
-			private static CarVIN readInputCarVin(InputReader inputReader) {
-				Faker faker = new Faker();
-				String VIN = faker.bothify("?????################").toUpperCase();
-        System.out.println("Generated VIN: " + VIN);
-				return new CarVIN( VIN);
-			}
 
 }
