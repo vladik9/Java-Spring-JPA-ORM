402f40fbb9f5843e6c526395e35815d128135c61
Vlad <v.cornici@gmail.com>
2024-06-15 13:39:16 +0300

    updated utils

--- src/main/java/com/example/Utils/Utils.java
+++ src/main/java/com/example/Utils/Utils.java
@@ -1,5 +1,10 @@
 package com.example.Utils;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import com.example.app.InputReader;
 import com.example.app.Car.Car;
 import com.example.app.Destination.Destination;
@@ -9,11 +14,6 @@ import com.example.app.Passenger.Passenger;
 import com.example.app.Ride.Ride;
 import com.example.app.Vin.Vin;
 import com.github.javafaker.Faker;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 
 public class Utils {
   // The line `public static final CarBrand[] CAR_BRANDS = CarBrand.values();` is
@@ -67,6 +67,12 @@ public class Utils {
     return Utils.CAR_BRANDS[index].toString();
   }
 
+  /**
+   * Generates a list of random LicenseEnum objects representing different driving
+   * categories.
+   *
+   * @return a list of LicenseEnum objects representing driving categories
+   */
   public static List<LicenseEnum> generateLicenseCategory() {
     List<LicenseEnum> drivingCategories = new ArrayList<LicenseEnum>();
     for (int i = 0; i < 4; i++) {
@@ -78,25 +84,56 @@ public class Utils {
 
   // ##################################
   // generator of Cars, Drivers and Vin's
+  /**
+   * Generates a new Car object with randomly generated car name, color, year, and
+   * number of doors.
+   *
+   * @return a new Car object with randomly generated attributes
+   */
   public static Car generateCar() {
     return new Car(Utils.generateCarName(), faker.color().name().toUpperCase(),
         faker.number().numberBetween(1990, 2023 + 1), faker.number().numberBetween(2, 4 + 1));
   }
 
+  /**
+   * Generates a new Driver object with random name, age, and number of cars
+   * owned.
+   *
+   * @return a new Driver object with random name, age, and number of cars owned
+   */
   public static Driver generateDriver() {
     return new Driver(faker.name().fullName(), faker.number().numberBetween(18, 60),
         faker.number().numberBetween(2, 10));
   }
 
+  /**
+   * Generates a new Vin object with a randomly generated VIN number.
+   *
+   * @return a new Vin object with a randomly generated VIN number
+   */
   public static Vin generateVin() {
     return new Vin(faker.bothify("?????################").toUpperCase());
   }
 
+  /**
+   * Generates a new License object with a randomly generated license number,
+   * the current date, a date 10 years from now, and a randomly generated
+   * license category.
+   *
+   * @return a new License object with a randomly generated license number,
+   *         the current date, a date 10 years from now, and a randomly
+   *         generated license category.
+   */
   public static License generateLicenses() {
     return new License(faker.bothify("################"), LocalDate.now(),
         LocalDate.now().plusYears(10), generateLicenseCategory());
   }
 
+  /**
+   * Generates a new Passenger object with random name, age, and gender.
+   *
+   * @return a new Passenger object with random name, age, and gender
+   */
   public static Passenger generatePassenger() {
     return new Passenger(
         faker.name().fullName(),
@@ -104,12 +141,26 @@ public class Utils {
         genders[faker.random().nextInt(genders.length)].toString());
   }
 
+  /**
+   * Generates a new Ride object with a randomly generated price, distance, and
+   * destination.
+   *
+   * @return a new Ride object with a randomly generated price, distance, and
+   *         destination
+   */
   public static Ride generateRide() {
     double distance = faker.number().randomDouble(2, 0, 100);
     double price = distance * 1.5;
     return new Ride(price, distance, generateDestination());
   }
 
+  /**
+   * Generates a new Destination object with randomly generated longitude and
+   * latitude values.
+   *
+   * @return A new Destination object with randomly generated longitude and
+   *         latitude values.
+   */
   public static Destination generateDestination() {
     return new Destination(faker.address().longitude(), faker.address().latitude());
 
